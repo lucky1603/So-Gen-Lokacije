26d74b7ffc35080455fa0bc0f15232ee56364c82
lucky1603 <sinisa.ristic@gmail.com>
2016-09-26 22:05:29 +0200

    Add project files.

---
+++ So-Gen Lokacije/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using So_Gen_Lokacije.Resources;
+
+namespace So_Gen_Lokacije
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
